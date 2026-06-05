611501a292f36349b9d509c20de67b5fa25a1dcb
kyran-z <m.sarti@htvalue.it>
2026-06-05 19:44:44 +0200

    first commit

---
+++ data-final-fixes.lua
@@ -0,0 +1,64 @@
+recipe_list = {
+
+"animal-sample-01", "perfect-samples", --samples
+"aramid", "aramid-2", --all aramid
+"biofilm","biofilm-3", --all biofilms
+"nanofibrils", "nanofibrils-2", "nanofibrils-xyhiphoe", --all nanofibrils
+"chimeric-proteins", "cbp", "chitosan", "mutant-enzymes", --chitosan ingredients
+"science-coating", "coated-container", --advanced coating and containers
+"tbp", --tributyl phosphate
+"dedicated-oleochemicals", --oleo straight from titanium
+"cytostatics-2", -- can prod 1 but not 2???
+"laser-module", "laser-module-alt", --laser module
+"lfo", "chemistry", --lumogen
+"zno-nanoparticles", --zinc nanoparticles
+"guhcl", "pre-fiber-2", "pre-fiber-3", --part of the nanomesh chain
+"pre-enzyme", "crude-enzyme", "guaiacol", "mpa", "bt", "nanozymes", --part of the nanozymes chain
+"nickel-nitrate", --neodymium nitrate yes and this no??
+"ingap", --ingap, why not?
+"cage-antelope", --if normal cages are proddable, why not these?
+"nexelit-battery", "used-nexelit-battery", "personal-fusion-cell", "antimatter", -- all batteries
+"chloral", "mcb", "pre-pesticide-01", "pesticide-mk01", --pesticides 1
+"pesticide-mk02", "pre-pesticide-02", "ethyl-mercaptan","ethyl-mercaptan-2", "dda", --pesticides 2
+"dowfroth-250", "acid-strip-solution", --solvents
+"propene-to-acetone", "naphthalene-solvent", --very reasonable
+"ethylene-chlorohydrin", "chloroethanol", "ethylene-glycol", --the all chain
+"styrene","stopper-2","nylon-parts", --rubber things and adiacents
+"vonix-grow-01", -- special growth recipes are all proddable
+"hotair-shaft-mk02", "hotair-shaft-mk03", --won't work with shafts mk2 for some reason
+"fiberboard", "fiberboard-mk02", "fiberboard-3","phenolicboard", -- all boards
+"molten-thorium","th-oxide","th-dust", --thorium chain
+"bhoddos-spore","sporopollenin", --spores missing prod
+"nickel-carbonyl", --clear intermediate
+"liquid-air-distilation", "liquid-pure-air", "cold-clean-air", "cool-air", "purified-air", "gas-stream-filtration", --helium chain with no productivity for some reason
+"ammonium-sulfate", "ammonium-nitrate",
+"nv-center", --nitrogen vacancy center
+
+-- all other recipes in the chain can be prodded already
+"distilled-raw-coal",
+"molybdenum-filtration",
+"creamy-latex",
+"resveratrol",
+"dcm",
+"myoglobin",
+--
+
+-- all of these are kinda on the limit
+"gh", "gh-2", --growth hormones
+"antiviral", "antiviral-02",
+"reca", "reca-2",
+"orexigenic", "orexigenic-2",
+"bmp", "bmp-2",
+"anabolic-rna", "anabolic-rna-2",
+"aerogel",
+--
+
+--a bit cheaty perhaps
+"metastable-quasicrystal",
+"quantum-dots",
+"quantum-simulation-data","space-science-pack",
+"space-suit"
+--
+}
+
+py.allow_productivity(recipe_list)
